5479b56e473f0e98d4766d08aa348266f36feb12
Arinaivo Andriatsihafa Ranto Miarantsoa <andriatsihafamiarantsoa@gmail.com>
2026-05-13 11:32:09 +0300

    demandes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,4 +9,20 @@ $routes->get('/', 'Home::index');
 $routes->get('/produits', 'Produit::index');
 $routes->get('/produits/(:num)', 'Produit::show/$1');
 
+$routes->group('/rh', function ($routes) {
+    
+    // Demandes
+    $routes->get('demandes',           'RhController::showDemandeAttente');
+    $routes->get('approuver/(:num)',   'RhController::approuverConge/$1');
+    $routes->get('refuser/(:num)',     'RhController::refuserConge/$1');
+    $routes->get('annuler/(:num)',     'RhController::annulerConge/$1');
 
+    // Employés
+    $routes->get('employes',           'RhController::listeEmployes');
+    $routes->get('employes/(:num)',    'RhController::voirEmploye/$1');
+
+    // Soldes
+    $routes->get('soldes',             'RhController::listeSoldes');
+    $routes->get('soldes/(:num)',      'RhController::voirSolde/$1');
+
+});
